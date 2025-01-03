5abfd43f04d3bd594df8000d23527d756610cdef
Furkan <furkanalhan1210@gmail.com>
2025-01-03 23:36:29 +0100

    almost finished

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.ImageField(upload_to='carousel_img/', blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
